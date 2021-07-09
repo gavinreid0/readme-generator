a604399c288e3714f92ab0dd031dcf7ebf50fc16
Gavin Reid <gavinreid94@gmail.com>
2021-07-08 21:31:21 -0500

    created questions with inquirer & function to write readme file with fs

--- index.js
+++ index.js
@@ -4,10 +4,54 @@ const inquirer = require("inquirer");
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a brief description about your project: "
+    },
+    {
+        type: "input",
+        name: "link",
+        message: "Add a link to your deployed project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide instructions for a user to install & run this application: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Include instructions for how to use: "
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Select a license or select 'Unlicense': ",
+        choices: [
+            "Apache",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open Data Commons",
+            "Unlicense"
+        ]
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("README.md File Generated"));
+}
 
 // TODO: Create a function to initialize app
 function init() {}
